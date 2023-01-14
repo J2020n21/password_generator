00803572a43202980b52d10fe37bb8800e12714d
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-01-14 21:30:28 +0900

    Add files via upload
    initial commit with logo image

---
+++ PasswordManager/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+#  ------------------ UI SETUP ---------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+canvas = Canvas()
+canvas.config(width=200, height=200)
+
+window.mainloop()
